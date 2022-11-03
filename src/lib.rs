dc60de1c9ab9206306190cae59741e4f469aba0d
joeyame <josephameadows+git@gmail.com>
2022-11-02 22:29:08 -0700

    Updated proxy to have a smaller codebase with the same functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,94 +1,47 @@
 use poem::{
-    Endpoint, Request, Result, Response, Error, http::StatusCode, Body
+    Endpoint, Request, Result, Response, Error, http::StatusCode, Body,
+    handler, web::Data
 };
 
-#[derive(Debug)]
-pub struct ProxyEndpoint {
-    target: String,
-}
+#[handler]
+pub async fn proxy( req: &Request, target: Data<&String> ) -> Result<Response>
+{
+    let target = target.to_owned();
 
-impl ProxyEndpoint {
-    pub fn new( target: String ) -> Self {
-        Self { target: target }
-    }
-}
+    let path = req
+        .uri()
+        .path()
+        .trim_start_matches( (target.clone()+"/").as_str() )
+        .trim_end_matches( "/" );
+    
+    let mut request_path = target.clone() + path.clone();
+    println!( "Received request: {}", request_path );
 
-// async fn serve_target( req: &Request ) -> Result<Response> {
-//     Ok(
+    let client = reqwest::Client::new();
+    println!( "Request headers: {:?}", req.headers().clone() );
+    let res = client.get( request_path )
+        .headers( req.headers().clone() )
+        .body( "poopy" )
+        .send()
+        .await;
 
-//     )
-// }
-
-#[async_trait::async_trait]
-impl Endpoint for ProxyEndpoint {
-    type Output = Response;
-
-    async fn call( &self, req: Request ) -> Result<Self::Output> {
-
-        let path = req
-            .uri()
-            .path()
-            .trim_start_matches( (self.target.clone()+"/").as_str() )
-            .trim_end_matches( "/" );
-        
-        let mut request_path = self.target.clone() + path.clone();
-        println!( "Received request: {}", request_path );
-
-        let client = reqwest::Client::new();
-        println!( "Request headers: {:?}", req.headers().clone() );
-        let res = client.get( request_path )
-            .headers( req.headers().clone() )
-            .body( "poopy" )
-            .send()
-            .await;
-
-        match res {
-            Ok( result ) => {
-
-
-                let mut j = Response::default();
-                
-                // j.set_status( result.status() );
-                // j.set_body( result.text().await.unwrap() );
-                // j.set_body( result.text().await.unwrap() );
-                // let headers = j.headers_mut();
-
-                // *headers = result.headers().clone();
-                
-                // println!("Response: {:?}", &result.text().await );
-                    // status: result.status(),
-                    // headers: result.headers().clone(),
-                    // body: Body::from_string( result.text().await.unwrap() ),
-                    // ..Default::default()
-                    // j.head
-                // let l = result.extensions().clone_into( j.extensions_mut().get() );
-                j.extensions().clone_from( &result.extensions() );
-                // println!("Result extensions: {:?}", result.extensions() );
-                // j.headers().clone_from( &result.headers() );
-                // j.headers().extend( result.headers().get_all(key) );
-                result.headers().iter().for_each(|(key, val)| {
-                    j.headers_mut().insert( key, val.to_owned() );
-                });
-
-                j.set_status( result.status() );
-                j.set_version( result.version() );
-
-                j.set_body( result.bytes().await.unwrap() );
-
-                println!("j headers: {:?}", j.headers() );
-                // println!("Result status: {:?}", result.status() );
-                // println!("Result body: {:?}", result.bytes().await );
-                // println!("Result headers: {:?}", result );
-                    
-                Ok( j )
-            },
-            Err( error ) => {
-                println!( "ERROR!" );
-                Err( Error::from_string( error.to_string(), error.status().unwrap_or( StatusCode::EXPECTATION_FAILED ) ) )
-            }
+    match res {
+        Ok( result ) => {
+            let mut j = Response::default();
+            j.extensions().clone_from( &result.extensions() );
+            result.headers().iter().for_each(|(key, val)| {
+                j.headers_mut().insert( key, val.to_owned() );
+            });
+            j.set_status( result.status() );
+            j.set_version( result.version() );
+            j.set_body( result.bytes().await.unwrap() );
+            println!("j headers: {:?}", j.headers() );
+            Ok( j )
+        },
+        Err( error ) => {
+            println!( "ERROR!" );
+            Err( Error::from_string( error.to_string(), error.status().unwrap_or( StatusCode::EXPECTATION_FAILED ) ) )
         }
-
-        // return Err( Error::from_string( "Error!".to_owned(), StatusCode::NOT_IMPLEMENTED ) );
     }
 }
 
